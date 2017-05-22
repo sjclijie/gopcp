2b1145919b244183a5ca2f5660f5f9cb91e24e68
sjclijie <sjclijie@vip.qq.com>
2017-05-22 23:48:20 +0800

    update

--- socket_client.go
+++ socket_client.go
@@ -4,38 +4,126 @@ import (
 	"net"
 	"fmt"
 	"time"
+	"strings"
+	"math/rand"
 	"bytes"
 	"io"
+	"sync"
 )
+
+const (
+	SERVER_NETWORK = "tcp"
+	SERVER_ADDRESS = "127.0.0.1:9898"
+	DELIMITER      = '\t'
+	CLIENT_NUMS    = 1
+)
+
+var wg sync.WaitGroup
 
 func main() {
 
-	conn, err := net.DialTimeout("tcp", "127.0.0.1:9898", time.Second*10)
+	wg.Add(2)
 
-	if err != nil {
-		fmt.Printf("Dial error %s\n", err)
+	go request(1)
+
+	time.Sleep(time.Second * 2)
+
+	go request(2)
+
+	wg.Wait()
+}
+
+func printLog(role string, sn int, format string, args ...interface{}) {
+
+	if !strings.HasSuffix(format, "\n") {
+		format += "\n"
 	}
 
-	var dataBuffer bytes.Buffer
+	fmt.Printf("%s[%d]: %s", role, sn, fmt.Sprintf(format, args...))
+}
+
+func printClientLog(sn int, format string, args ...interface{}) {
+	printLog("Client", sn, format, args...)
+}
+
+func write(conn net.Conn, content string) (int, error) {
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString(content)
+	buffer.WriteByte(DELIMITER)
+
+	return conn.Write(buffer.Bytes())
+}
+
+func read(conn net.Conn) (string, error) {
+
+	readBytes := make([]byte, 1)
+
+	var buffer bytes.Buffer
 
 	for {
 
-		b := make([]byte, 10)
-
-		n, err := conn.Read(b)
+		_, err := conn.Read(readBytes)
 
 		if err != nil {
-			if err == io.EOF {
-				fmt.Printf("The connection is closed.")
-				conn.Close()
-			} else {
-				fmt.Printf("Read Error: %s", err)
-			}
+			return "", err
+		}
+
+		readByte := readBytes[0]
+
+		if readByte == DELIMITER {
 			break
 		}
 
-		dataBuffer.Write(b[:n])
+		buffer.WriteByte(readByte)
 	}
 
-	fmt.Printf(dataBuffer.String())
+	return buffer.String(), nil
+}
+
+func request(id int) {
+
+	conn, err := net.DialTimeout(SERVER_NETWORK, SERVER_ADDRESS, time.Second*10)
+
+	if err != nil {
+		printClientLog(id, "Dial error %s\n", err)
+	}
+
+	defer conn.Close()
+
+	printClientLog(id, "Connected to server.( remote address: %s, local address: %s)", conn.RemoteAddr(), conn.LocalAddr())
+
+	for i := 0; i < CLIENT_NUMS; i++ {
+
+		reqId := rand.Int31()
+
+		n, err := write(conn, fmt.Sprintf("%d", reqId))
+
+		if err != nil {
+			printClientLog(id, "Writer Error: ", err)
+			continue
+		}
+
+		printClientLog(id, "Sent request ( written %d bytes ): %d.", n, reqId)
+
+		time.Sleep(200 * time.Microsecond)
+	}
+
+	for i := 0; i < CLIENT_NUMS; i++ {
+
+		strResp, err := read(conn)
+
+		if err != nil {
+			if err == io.EOF {
+				printClientLog(id, "The connection is closed by another side")
+			} else {
+				printClientLog(id, "Read Error: %s", err)
+			}
+		}
+
+		printClientLog(id, "Response: %s", strResp)
+	}
+
+	wg.Done()
 }
--- socket_server.go
+++ socket_server.go
@@ -3,25 +3,162 @@ package main
 import (
 	"net"
 	"fmt"
-	"bufio"
+	"strings"
+	"time"
+	"bytes"
+	"io"
+	"strconv"
+	"math"
+)
+
+const (
+	SERVER_NETWORK = "tcp"
+	SERVER_ADDRESS = "127.0.0.1:9898"
+	DELIMITER      = '\t'
 )
 
 func main() {
 
-	listener, err := net.Listen("tcp", "127.0.0.1:9898")
+	listener, err := net.Listen(SERVER_NETWORK, SERVER_ADDRESS)
 
 	if err != nil {
-		fmt.Printf("Listen error: %s\n", err)
+		printServerLog("Listen Error: %s", err)
+		return
 	}
 
+	defer listener.Close()
+
+	printServerLog("Got listener for the server: (local address: %s)", listener.Addr())
+
+	for {
+
+		conn, err := listener.Accept()
+
+		if err != nil {
+			printServerLog("Accept Error: %s", err)
+		}
+
+		printServerLog("Established a connection with a client application. ( remote address: %s )", conn.RemoteAddr())
+
+		go handleConn(conn)
+	}
+
+	/*
 	conn, err := listener.Accept()
+
+	fmt.Println(conn.LocalAddr().Network())
+	fmt.Println(conn.RemoteAddr().Network())
 
 	if err != nil {
 		fmt.Printf("Accept error: %s\n", err)
 	}
 
-	writer := bufio.NewWriter(conn)
+	//writer := bufio.NewWriter(conn)
+	*/
+}
 
+func handleConn(conn net.Conn) {
 
+	for {
 
+		conn.SetReadDeadline(time.Now().Add(time.Second * 2))
+
+		strReq, err := read(conn)
+
+		if err != nil {
+
+			if err == io.EOF {
+				printServerLog("The connection is closed by anthoer side.")
+			} else {
+				printServerLog("Read Error: %s", err)
+			}
+
+			break
+		}
+
+		printServerLog("Received request: %s", strReq)
+
+		intReq, err := strToint32(strReq)
+
+		if err != nil {
+			n, err := write(conn, err.Error())
+			printServerLog("Sent error message ( written %d bytes ): %s", n, err)
+			continue
+		}
+
+		floatResp := math.Cbrt(float64(intReq))
+
+		respMsg := fmt.Sprintf("the cube root of %d is %f", intReq, floatResp)
+
+		n, err := write(conn, respMsg)
+
+		if err != nil {
+			printServerLog("Writ Error: %s", err)
+		}
+
+		printServerLog("Sent Response ( written %d bytes ): %s", n, respMsg)
+	}
+}
+
+func write(conn net.Conn, content string) (n int, err error) {
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString(content)
+	buffer.WriteByte(DELIMITER)
+
+	return conn.Write(buffer.Bytes())
+}
+
+func strToint32(str string) (int32, error) {
+
+	num, err := strconv.ParseInt(str, 10, 0)
+
+	if err != nil {
+		return 0, fmt.Errorf("%s is not integer", str)
+	}
+
+	if num > math.MaxInt32 || num < math.MinInt32 {
+		return 0, fmt.Errorf("%d is not 32-bit integer", num)
+	}
+
+	return int32(num), nil
+}
+
+func read(conn net.Conn) (string, error) {
+
+	readBytes := make([]byte, 1)
+
+	var buffer bytes.Buffer
+
+	for {
+		_, err := conn.Read(readBytes)
+
+		if err != nil {
+			return "", err
+		}
+
+		readByte := readBytes[0]
+
+		if readByte == DELIMITER {
+			break
+		}
+
+		buffer.WriteByte(readByte)
+	}
+
+	return buffer.String(), nil
+}
+
+func printLog(role string, sn int, format string, args ...interface{}) {
+
+	if !strings.HasSuffix(format, "\n") {
+		format += "\n"
+	}
+
+	fmt.Printf("%s[%d]: %s", role, sn, fmt.Sprintf(format, args...))
+}
+
+func printServerLog(format string, args ...interface{}) {
+	printLog("Server", 0, format, args...)
 }
